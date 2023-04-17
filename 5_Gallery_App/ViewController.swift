029adf7e23729d812671d0d3fea4a482c184401a
AntonyBil <beletskiy.ant@gmail.com>
2023-04-17 19:44:02 +0300

    Initial commit

---
+++ 5_Gallery_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5_Gallery_App
+//
+//  Created by apple on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
